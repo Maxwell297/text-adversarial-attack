d5bf04236b5fcfb7d478b224d2de92c0edf71b69
Jiarui Yao <jryao@cml24.umiacs.umd.edu>
2023-06-28 22:11:11 -0400

    add text generation attack

--- text_generation_roberta.py
+++ text_generation_roberta.py
@@ -2,7 +2,7 @@ from transformers import (RobertaTokenizer,
     RobertaForSequenceClassification,
     pipeline, AutoModelForCausalLM,
     GPT2TokenizerFast, GPT2LMHeadModel)
-import jsonlines
+import jsonlines, json
 import jiwer
 import torch
 import torch.nn.functional as F
@@ -66,18 +66,20 @@ prompts = []
 prompts_num = 100
 start_index = 0
 num_samples = 100
-num_iters = 100
+num_iters = 5000
 adv_loss = 'cw'
 initial_coeff = 15
-lr = 1
+lr = 2e-1
 batch_size = 1
 embed_layer = -1
-lam_sim = 1
-lam_perp = 1
-gumbel_samples = 10
+lam_sim = 0.00
+lam_perp = 0.00
+gumbel_samples = 100
 kappa = 5
 max_new_tokens = 30
-print_every = 1
+print_every = 50
+topk_attack = 3
+early_stop_adv_loss = 0.2
 
 # generator = pipeline("text-generation", model=model)
 generate_tokenizer = GPT2TokenizerFast.from_pretrained(model)
@@ -112,6 +114,19 @@ times = []
 end_index = min(start_index + num_samples, len(encoded_dataset))
 adv_losses, ref_losses, perp_losses, entropies = torch.zeros(end_index - start_index, num_iters), torch.zeros(end_index - start_index, num_iters), torch.zeros(end_index - start_index, num_iters), torch.zeros(end_index - start_index, num_iters)
 
+dirty_words = []
+dirty_words_ids = []
+with open('data/profane_words.json', 'r') as f:
+    dirty_words_ = json.load(f)
+encoded_dirty_words = generate_tokenizer(dirty_words_)
+
+for i in range(len(dirty_words_)):
+    if len(encoded_dirty_words['input_ids'][i]) == 1:
+        dirty_words.append(dirty_words_[i])
+        dirty_words_ids.append(encoded_dirty_words['input_ids'][i][0])
+print(dirty_words)
+attack_succ = 0
+
 for idx in range(start_index, end_index):
     input_ids = torch.tensor(encoded_dataset['input_ids'][idx])
     prompt = encoded_dataset['prompt'][idx]
@@ -127,8 +142,6 @@ for idx in range(start_index, end_index):
     # adv_loss = 1.0 - torch.softmax(clean_toxicity, 0)[1]
 
     forbidden = np.zeros(len(input_ids)).astype('bool')
-    forbidden[0] = True
-    forbidden[-1] = True
     forbidden_indices = np.arange(0, len(input_ids))[forbidden]
     forbidden_indices = torch.from_numpy(forbidden_indices).cuda()
     token_type_ids_batch = None
@@ -144,13 +157,48 @@ for idx in range(start_index, end_index):
         log_coeffs = log_coeffs.cuda()
         log_coeffs.requires_grad = True
 
-    optimizer = torch.optim.Adam([log_coeffs], lr=lr)
+    # optimizer = torch.optim.Adam([log_coeffs], lr=lr)
     start = time.time()
+
+    # 寻找gradient最大的token作为attack target
+    # optimizer.zero_grad()
+    tmp = log_coeffs.unsqueeze(0).repeat(batch_size, 1, 1)
+    coeffs = my_gumbel_softmax(tmp, hard=False)
+    inputs_embeds = (coeffs @ embeddings[None, :, :])
+    pred = generate_model(inputs_embeds=inputs_embeds).logits
+    pred_mask = generate_tokenizer.decode(pred.argmax(dim=-1).squeeze()[-1])
+    pred_mask_prob = pred[0, -1].softmax(0)
+    adv_loss = 1.0 - pred_mask_prob[dirty_words_ids].sum()
+    ref_embeds = (coeffs @ ref_embeddings[None, :, :])
+    pred = ref_model(inputs_embeds=ref_embeds)
+    if lam_sim > 0:
+        output = pred.hidden_states[embed_layer]
+        ref_loss = -lam_sim * bert_score(orig_output, output, weights=ref_weights).mean()
+    else:
+        ref_loss = 0.0
+    if lam_perp > 0:
+        perp_loss = lam_perp * log_perplexity(pred.logits, coeffs)
+    else:
+        perp_loss = 0.0
+    total_loss = adv_loss + ref_loss + perp_loss
+    total_loss.backward()
+    log_coeffs.grad.index_fill_(0, forbidden_indices, 0)
+    _, attack_indices = torch.topk(torch.norm(log_coeffs.grad, dim=1),topk_attack)
+    log_coeffs.grad.index_fill_(0, torch.arange(len(input_ids)).cuda(), 0)
+    forbidden = torch.from_numpy(np.ones(len(input_ids)).astype('bool')).cuda()
+    forbidden[attack_indices] = False
+    forbidden_indices = torch.arange(len(input_ids)).cuda()[forbidden]
+    log_coeffs.requires_grad = False
+    log_coeffs[forbidden_indices, torch.tensor(input_ids).cuda()[forbidden_indices]] = 100
+    log_coeffs.requires_grad = True
+    print('Attack indices:', attack_indices)
+    optimizer = torch.optim.Adam([log_coeffs], lr=lr)
+
     for i in range(num_iters):
-        for plt_id in range(len(input_ids)):
-            plt.scatter(np.arange(generate_tokenizer.vocab_size),log_coeffs[plt_id].cpu().detach().numpy())
-            plt.savefig(f'figs/{i}-{plt_id}-pure_adv.png')
-            plt.clf()
+        # for plt_id in range(len(input_ids)):
+        #     plt.scatter(np.arange(generate_tokenizer.vocab_size),log_coeffs[plt_id].cpu().detach().numpy())
+        #     plt.savefig(f'figs/{i}-{plt_id}-pure_adv.png')
+        #     plt.clf()
         # b0 = time.time()
         optimizer.zero_grad()
         tmp = log_coeffs.unsqueeze(0).repeat(batch_size, 1, 1)
@@ -158,17 +206,19 @@ for idx in range(start_index, end_index):
         inputs_embeds = (coeffs @ embeddings[None, :, :]) # 1*21*768
         # e1 = time.time()
         # FIXME: 目前bug: generate_model.generate()会丢失梯度信息，导致adv_loss.backward无法back propagate到log_coeffs
-        pred = generate_model.generate(inputs_embeds=inputs_embeds, max_new_tokens=max_new_tokens)
-        pred = generate_tokenizer.batch_decode(pred)
-        pred = [pred_[13:] for pred_ in pred]
+        pred = generate_model(inputs_embeds=inputs_embeds)
+        pred_next = generate_tokenizer.decode(pred.logits.argmax(-1)[0,-1])
+        adv_loss = 1.0 - pred.logits[0,-1].softmax(0)[dirty_words_ids].sum()
+        # pred = generate_tokenizer.batch_decode(pred)
+        # pred = [pred_[13:] for pred_ in pred]
         # origin_prompt = generate_tokenizer.batch_decode(generate_model.generate(inputs_embeds=inputs_embeds, max_new_tokens=len(input_ids)))
         # pred = [pred_[len(origin_prompt_):] for pred_, origin_prompt_ in zip(pred, origin_prompt)]
         # print('Sample prompt:', origin_prompt)
-        print('Sample text:', pred)
+        # print('Sample text:', pred)
         # e2 = time.time()
-        toxicity_input = toxicity_tokenizer(pred, padding=True, truncation=True, return_tensors='pt')
-        toxicity_logits = toxicity_model(**toxicity_input).logits
-        adv_loss = (toxicity_logits[:,0] - toxicity_logits[:,1] + kappa).clamp(min=0).mean()
+        # toxicity_input = toxicity_tokenizer(pred, padding=True, truncation=True, return_tensors='pt')
+        # toxicity_logits = toxicity_model(**toxicity_input).logits
+        # adv_loss = (toxicity_logits[:,0] - toxicity_logits[:,1] + kappa).clamp(min=0).mean()
         # e3 = time.time()
         # print(f'TIME: coeff {e1-b0}, generate {e2-e1}, toxicity {e3-e2}')
 
@@ -180,10 +230,14 @@ for idx in range(start_index, end_index):
         if lam_sim > 0:
             output = pred.hidden_states[embed_layer]
             ref_loss = -lam_sim * bert_score(orig_output, output, weights=ref_weights).mean()
+        else:
+            ref_loss = 0.0
         # e5 = time.time()
         # (log) perplexity constraint
         if lam_perp > 0:
             perp_loss = lam_perp * log_perplexity(pred.logits, coeffs)
+        else:
+            perp_loss = 0.0
         # e6 = time.time()
         # print(f'TIME: ref {e4-e3}, bert {e5-e4}, ppl {e6-e5}')
         total_loss = adv_loss + ref_loss + perp_loss
@@ -196,11 +250,14 @@ for idx in range(start_index, end_index):
         # e8 = time.time()
         if i % print_every == 0:
             print(f'Iteration: {i+1}, total loss: {total_loss}, adv_loss: {adv_loss}, ref_loss: {ref_loss}, perp_loss: {perp_loss}, entropy: {entropy}, time: {time.time()-start}')
+            print(log_coeffs.grad.sum())
+            print('Sample text:', pred_next)
             # print(f'Iteration: {i+1}, total (adv) loss: {adv_loss}, entropy: {entropy}, time: {time.time()-start}')
             # print(log_coeffs.shape)
             # print('-'*50)
             # print(coeffs.shape)
-
+        if adv_loss < early_stop_adv_loss:
+            break
 
         # Gradient step
         log_coeffs.grad.index_fill_(0, forbidden_indices, 0)
@@ -209,10 +266,10 @@ for idx in range(start_index, end_index):
         # print(f'TIME: backward {e7-e6}, entropy {e8-e7}, step {e9-e8}')
 
         # Log statistics
-        adv_losses[idx-start_index, i] = adv_loss.detach().item()
-        ref_losses[idx-start_index, i] = ref_loss.detach().item()
-        perp_losses[idx-start_index, i] = perp_loss.detach().item()
-        entropies[idx-start_index, i] = entropy.detach().item()
+        # adv_losses[idx-start_index, i] = adv_loss.detach().item()
+        # ref_losses[idx-start_index, i] = ref_loss.detach().item()
+        # perp_losses[idx-start_index, i] = perp_loss.detach().item()
+        # entropies[idx-start_index, i] = entropy.detach().item()
     
     times.append(time.time() - start_time)
 
@@ -221,21 +278,31 @@ for idx in range(start_index, end_index):
             adv_ids = my_gumbel_softmax(log_coeffs, hard=True).argmax(1)
             adv_ids = adv_ids.cpu().tolist()
             adv_text = generate_tokenizer.decode(adv_ids)
+            perturbed_prompt = adv_text
             x = generate_tokenizer(adv_text, max_length=256, truncation=True, return_tensors='pt')
             token_errors.append(wer(adv_ids, x['input_ids'][0]))
             adv_generate_text = generate_model.generate(input_ids=x['input_ids'].cuda(), attention_mask=x['attention_mask'].cuda(),
                                     max_new_tokens=max_new_tokens)
+            adv_generate_word = generate_tokenizer.decode(generate_model(input_ids=x['input_ids'].cuda()).logits[0,-1].argmax(0))
             adv_generate_text = generate_tokenizer.batch_decode(adv_generate_text)
             adv_generate_text = [adv_generate_text[0][len(adv_text):]]
             adv_toxicity = toxicity_model(**toxicity_tokenizer(adv_generate_text, padding=True, truncation=True, return_tensors='pt')).logits[0]
             adv_toxicity = F.softmax(adv_toxicity)[1]
-            if adv_toxicity > 0.5 or j == gumbel_samples - 1:
-                print('Adv generated text', adv_generate_text)
+            if adv_generate_word.strip() in dirty_words or j == gumbel_samples - 1:
+                print('Perturbed prompt:', perturbed_prompt)
+                print('Adv predicted word:', adv_generate_word)
+                print('Adv generated text:', adv_generate_text)
                 print('Adv toxicity:', adv_toxicity)
+                if adv_generate_word.strip() in dirty_words:
+                    print('Attack succeed')
+                    attack_succ += 1
                 break
 
     # print(adv_loss)
     exit(0)
+
+print('Successful attack rate:', attack_succ / (end_index - start_index))
+print('Total prompts:', end_index - start_index)
 
 # with open(prompt_file, 'r') as f:
 #     prompt_cnt = 0
